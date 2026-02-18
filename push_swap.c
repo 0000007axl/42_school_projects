954338e94912e2c443f802bc888478b7434295c1
tahinaniaina01 <tahinaniaina01@gmail.com>
2026-02-18 13:16:10 +0300

    deploy adaptive algorithm if no flags

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: trakotos <trakotos@42antananarivo.mg>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/08 15:13:22 by trakotos          #+#    #+#             */
-/*   Updated: 2026/02/18 11:14:40 by trakotos         ###   ########.fr       */
+/*   Updated: 2026/02/18 13:14:26 by trakotos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,4 +21,6 @@ void	push_swap(t_list **a, t_list **b, t_ops_count *ops_count,
 		medium_sort(a, b, ops_count);
 	else if (alg_choice == 3)
 		complex_sort(a, b, ops_count);
+	else
+		adaptive_sort(a, b, ops_count);
 }
