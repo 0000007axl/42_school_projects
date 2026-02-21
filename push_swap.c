b3646b2257bf49edd091f55bc3e1da7130658053
tahinaniaina01 <tahinaniaina01@gmail.com>
2026-02-21 09:51:50 +0300

    fix: add lst_clear_ops function for proper memory management in push_swap

--- push_swap.c
+++ push_swap.c
@@ -6,11 +6,28 @@
 /*   By: trakotos <trakotos@42antananarivo.mg>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/08 15:13:22 by trakotos          #+#    #+#             */
-/*   Updated: 2026/02/20 13:17:42 by trakotos         ###   ########.fr       */
+/*   Updated: 2026/02/21 09:50:21 by trakotos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+
+static t_ops	*lst_clear_ops(t_ops **lst)
+{
+	t_ops	*tmp;
+
+	if (lst == NULL)
+		return (NULL);
+	while (*lst != NULL)
+	{
+		tmp = *lst;
+		*lst = (*lst)->next;
+		tmp->next = NULL;
+		free(tmp);
+	}
+	*lst = NULL;
+	return (*lst);
+}
 
 void	push_swap(t_list **a, t_list **b, t_ops_count *ops_count,
 		int alg_choice)
@@ -31,4 +48,5 @@ void	push_swap(t_list **a, t_list **b, t_ops_count *ops_count,
 	else if (alg_choice == 3)
 		complex_sort(a, b, ops_count, &lst_ops);
 	print_ops(lst_ops);
+	lst_clear_ops(&lst_ops);
 }
